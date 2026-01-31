7554c7961817be2aab3e141b61ae50777234cd3a
amritashwarlal <amritasshwar@gmail.com>
2026-01-30 21:52:13 -0800

    Replace Next.js with simple HTML page - Hello World

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-black flex items-center justify-center">
-      <h1 className="text-white text-6xl font-bold">Lalita</h1>
-    </main>
-  );
-}
